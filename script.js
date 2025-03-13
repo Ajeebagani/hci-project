bec0f1a9535a9db6fc01d93a4d4e9e53b96ab39b
Ajeebagani <ajeebagani@gmail.com>
2025-03-14 00:00:20 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+document.addEventListener("DOMContentLoaded", () => {
+    const text = document.querySelector(".welcome-text");
+
+    setTimeout(() => {
+        text.style.color = "#ff6600"; // Color change for extra animation
+    }, 2000);
+});
